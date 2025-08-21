e1f60361b64dd45c2f692b46f42aca1211cb4687
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-08-20 20:51:21 -0700

    ContainsDuplicate217

---
+++ src/LeetCode/ContainsDuplicate217.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+public class ContainsDuplicate217 {
+    public static void main(String[] args) {
+        ContainsDuplicate217 obj = new ContainsDuplicate217();
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(obj.containsDuplicate(nums));
+    }
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!seen.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
