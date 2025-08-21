db41598e16ddc2f8336163f61107daa022f38d5e
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-08-20 20:33:59 -0700

    ClimbStairs.java

---
+++ src/LeetCode/ClimbStairs.java
@@ -0,0 +1,18 @@
+public class ClimbStairs {
+    public static void main(String[] args) {
+        System.out.println(ClimbStairs(4));
+    }
+
+    public static int ClimbStairs(int n) {
+        if (n <= 2) return n;
+        int AllWays = 0;
+        int OneStepBefore = 2;
+        int TwoStepbefore = 1;
+        for (int i = 3; i <= n; i++) {
+            AllWays = OneStepBefore + TwoStepbefore;
+            TwoStepbefore = OneStepBefore;
+            OneStepBefore = AllWays;
+        }
+        return AllWays;
+    }
+}
