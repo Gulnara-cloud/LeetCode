c74718a755395dd7b319a34f96274992b6006096
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-08-21 07:26:16 -0700

    MoveZeroes283

---
+++ src/LeetCode/MoveZeroes283.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class MoveZeroes283 {
+    public static void main(String[] args) {
+            int[] nums = {0, 1, 0, 3, 12};
+            moveZeroes(nums);
+            System.out.println(Arrays.toString(nums));
+        }
+
+        public static void moveZeroes(int[] nums) {
+            int left = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != 0) {
+                    int temp = nums[i];
+                    nums[i] = nums[left];
+                    nums[left] = temp;
+                    left++;
+                }
+            }
+        }
+    }
