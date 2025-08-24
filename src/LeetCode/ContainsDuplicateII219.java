f679cb21b02eb5b86f6ceabb8a749844b0d33799
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-08-24 11:47:07 -0700

    ContainsDuplicateII219

---
+++ src/LeetCode/ContainsDuplicateII219.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class ContainsDuplicateII219 {
+    public static boolean containsDuplicateII219(int[] nums, int k) {
+        HashMap<Integer, Integer> hs = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hs.containsKey(nums[i])) {
+                if (i - hs.get(nums[i]) <= k) {
+                    return true;
+                }
+            }
+            hs.put(nums[i], i);
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        int k = 3;
+        System.out.println(containsDuplicateII219(nums, k));
+    }
+}
+
