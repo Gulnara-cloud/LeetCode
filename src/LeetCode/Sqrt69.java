88289c961bc192c4e7efab55980392890c325ffe
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-08-26 22:13:50 -0700

    Sqrt69

---
+++ src/LeetCode/Sqrt69.java
@@ -0,0 +1,22 @@
+public class Sqrt69 {
+    public static void main(String[] args) {
+        int x = 4;
+        int left = 0;
+        int right = x;
+        while (left + 1 < right) {
+            long mid = (left + right) / 2;
+            if (mid * mid <= x) {
+                left = (int) mid;
+            } else {
+                right = (int) mid;
+            }
+        }
+        long mid = right;
+        if (mid * mid <= x) {
+            left = (int) mid;
+        }
+        System.out.println(left);
+    }
+}
+
+
