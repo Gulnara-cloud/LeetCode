bf907f23110ad9a9a4d2c1425b56da231e2e66de
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-08-27 18:43:41 -0700

    RemoveElement27

---
+++ src/LeetCode/RemoveElement27.java
@@ -0,0 +1,20 @@
+public class RemoveElement27 {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+    public static void main(String[] args) {
+        RemoveElement27 obj = new RemoveElement27();
+        int[] nums = {3, 2, 2, 3};
+        int result = obj.removeElement(nums, 3);
+        System.out.println(result);
+    }
+}
+
+
