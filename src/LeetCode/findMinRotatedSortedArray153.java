c86ad2bac0b5e480b4603d1a50a9ce239d656d96
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-09-03 19:20:22 -0700

    findMinRotatedSortedArray153

---
+++ src/LeetCode/findMinRotatedSortedArray153.java
@@ -0,0 +1,22 @@
+
+public class findMinRotatedSortedArray153 {
+    public static int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+    public static void main(String[] args) {
+        int[] array = {3, 4, 5, 1, 2};
+        System.out.println(findMinRotatedSortedArray153.findMin(array));
+    }
+}
+
