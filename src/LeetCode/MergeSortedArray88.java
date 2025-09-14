ab354286bd989e93531038c7d816374684dc43ba
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-09-14 12:13:33 -0700

    88

---
+++ src/LeetCode/MergeSortedArray88.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class MergeSortedArray88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        MergeSortedArray88 obj = new MergeSortedArray88();
+        obj.merge(nums1,3, nums2,3);
+        System.out.println(Arrays.toString(nums1));
+    }
+}
+
