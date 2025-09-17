e9b078e503e29c658da732777687e7cafb83e391
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-09-16 17:24:39 -0700

    922

---
+++ src/LeetCode/SortArrayByParity922.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class SortArrayByParity922 {
+    public int[] sortArrayByParityII(int[] nums) {
+        int i = 0;
+        int j = 1;
+        int n = nums.length;
+
+        while (i < n && j < n) {
+            while (i < n && nums[i] % 2 == 0) {
+                i += 2;
+            }
+            while (j < n && nums[j] % 2 == 1) {
+                j += 2;
+            }
+            if (i < n && j < n) {
+                swap(nums, i, j);
+            }
+        }
+        return nums;
+    }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public static void main(String[] args) {
+        int[] array = {4,2,5,7};
+        SortArrayByParity922 obj = new SortArrayByParity922();
+        System.out.println(Arrays.toString(array));
+    }
+}
