c184a7f33ca73c716592cc02be4dd2de2d7f9251
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-09-17 18:39:44 -0700

    128

--- src/LeetCode/LongestConsecutive128.java
+++ src/LeetCode/LongestConsecutive128.java
@@ -31,7 +31,7 @@ public class LongestConsecutive128 {
         return longestLength;
     }
     public static void main(String[] args) {
-        int[] nums = {100, 4, 200, 1, 3};
+        int[] nums = {100, 4, 200, 1, 3, 2};
         System.out.println(longestConsecutive(nums));
         // It skips 2, 3, 4 because they are not sequence starts.
         // It starts a sequence at 1: → 1, 2, 3, 4 → length = 4
