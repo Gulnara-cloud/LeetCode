5fc15cd6eb983859d3df36d478d6ec6dac026cb8
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-09-19 15:55:40 -0700

    704

---
+++ src/LeetCode/BinarySearch704.java
@@ -0,0 +1,25 @@
+public class BinarySearch704 {
+    public static int search(int[] nums, int target) {
+        int len = nums.length;
+        int left = 0;
+        int right = len - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        System.out.println(search(nums, 9));
+    }
+}
+
