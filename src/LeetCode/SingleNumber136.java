7a83cb013f0378a744ad2097b37555f5a37aedf8
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-09-20 19:02:33 -0700

    136

--- src/LeetCode/SingleNumber136.java
+++ src/LeetCode/SingleNumber136.java
@@ -1,6 +1,6 @@
 public class SingleNumber136 {
     public static void main(String[] args) {
-        int[] nums = {3, 2, 1};
+        int[] nums = {2, 2, 1};
         int unique = 0;
         for (int num : nums) {
             unique ^= num;
