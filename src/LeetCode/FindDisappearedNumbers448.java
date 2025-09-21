e7156602517ce344564413941ce72b84128a1d56
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-09-20 18:56:39 -0700

    448

--- src/LeetCode/FindDisappearedNumbers448.java
+++ src/LeetCode/FindDisappearedNumbers448.java
@@ -18,7 +18,7 @@ public class FindDisappearedNumbers448 {
         return result;
     }
     public static void main(String[] args) {
-        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+        int[] nums = {4, 3, 2, 7, 8, 2, 3, 0};
         System.out.println(findDisappearedNumbers(nums));
     }
 }
