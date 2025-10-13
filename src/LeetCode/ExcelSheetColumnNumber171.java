08884314be6b113bf90365dc86f52e12326739d9
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-10-13 13:22:15 -0700

    171

---
+++ src/LeetCode/ExcelSheetColumnNumber171.java
@@ -0,0 +1,18 @@
+public class ExcelSheetColumnNumber171 {
+    public static int titleToNumber(String columnTitle) {
+        int result = 0;
+        for (int i = 0; i < columnTitle.length(); i++) {
+            char c = columnTitle.charAt(i);
+            int value = c - 'A' + 1;
+            result = result * 26 + value;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String columnTitle = "AB";
+        int result = titleToNumber(columnTitle);
+        System.out.println(result);
+    }
+}
+
