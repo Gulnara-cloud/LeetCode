dd3ccf5e284433f864529615e2ec795c4dd66b95
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-11-13 13:10:39 -0800

    53

--- src/LeetCode/MaximumSubArray53.java
+++ src/LeetCode/MaximumSubArray53.java
@@ -1,5 +1,6 @@
 public class MaximumSubArray53 {
     public static int maxSubArray(int[] nums) {
+
         int currentSum = nums[0];
         int maxSum = nums[0];
         for (int i = 1; i < nums.length; i++) {
@@ -10,7 +11,7 @@ public class MaximumSubArray53 {
     }
 
     public static void main(String[] args) {
-        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        int[] nums = {5, 4, -1, 7, 8};
         System.out.println(maxSubArray(nums));
     }
 }
