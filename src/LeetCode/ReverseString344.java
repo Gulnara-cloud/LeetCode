bbc79c282c98bed50fc092d8a6fec2033947f26b
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-11-18 16:51:27 -0800

    344

--- src/LeetCode/ReverseString344.java
+++ src/LeetCode/ReverseString344.java
@@ -1,19 +1,19 @@
 public class ReverseString344 {
     public static void reverseString(char[] s) {
-        helper (s,0,s.length - 1);
-    }
-    private static void helper(char[] s, int left, int right) {
-        if(left >= right) {
-            return;
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            char temp = s[right];
+            s[right] = s[left];
+            s[left] = temp;
+            right--;
+            left++;
         }
-        char temp = s[left];
-        s[left] = s[right];
-        s[right] = temp;
-        helper (s,left + 1,right - 1);
     }
 
     public static void main(String[] args) {
-        char[] s = {'h','e','l','l','o'};
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
         reverseString(s);
         System.out.println(s);
     }
