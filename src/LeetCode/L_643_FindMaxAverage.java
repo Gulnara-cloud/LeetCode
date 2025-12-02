6c2efde2bdc16025713eb6df29f9fff2a63c3ca2
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-12-02 10:46:26 -0800

    643

--- src/LeetCode/L_643_FindMaxAverage.java
+++ src/LeetCode/L_643_FindMaxAverage.java
@@ -1,4 +1,4 @@
-public class FindMaxAverage643 {
+public class L_643_FindMaxAverage {
     public double findMaxAverage(int[] nums, int k) {
         int sum = 0;
         for (int i = 0; i < k; i++) {
@@ -13,7 +13,7 @@ public class FindMaxAverage643 {
     }
     public static void main(String[] args) {
         int[] nums = {1, 12, -5, -6, 50, 3};
-        FindMaxAverage643 obj = new FindMaxAverage643();
+        L_643_FindMaxAverage obj = new L_643_FindMaxAverage();
         double result = obj.findMaxAverage(nums, 4);
         System.out.println(result);
     }
