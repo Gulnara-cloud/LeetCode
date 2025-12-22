31fc5d0c36e4fc746435d150a36f03776769985b
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-12-21 20:44:56 -0800

    141

---
+++ src/LeetCode/L_141_LinkedListCycle.java
@@ -0,0 +1,36 @@
+public class L_141_LinkedListCycle {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+    public static boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        // 3, 2, 0, -4
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+
+        head.next.next.next.next = head.next;
+        System.out.println(hasCycle(head)); // true
+    }
+}
