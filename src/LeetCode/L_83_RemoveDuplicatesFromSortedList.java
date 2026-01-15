190a8d8428fdd6e22d9e0e6f6b378c853f3ac1bb
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-01-14 19:55:43 -0800

    83

--- src/LeetCode/L_83_RemoveDuplicatesFromSortedList.java
+++ src/LeetCode/L_83_RemoveDuplicatesFromSortedList.java
@@ -1,24 +1,18 @@
 public class L_83_RemoveDuplicatesFromSortedList {
 
-    // Внутренний класс узла списка
     static class ListNode {
         int val;
         ListNode next;
-
         ListNode() {
         }
-
         ListNode(int val) {
             this.val = val;
         }
-
         ListNode(int val, ListNode next) {
             this.val = val;
             this.next = next;
         }
     }
-
-    // Само решение задачи
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null || head.next == null) {
             return head;
@@ -27,19 +21,14 @@ public class L_83_RemoveDuplicatesFromSortedList {
 
         while (current != null && current.next != null) {
             if (current.val == current.next.val) {
-                // пропускаем дубликат
                 current.next = current.next.next;
             } else {
-                // переходим к следующему узлу
                 current = current.next;
             }
         }
-
         return head;
     }
-
     public static void main(String[] args) {
-        // создаём список: 1 -> 1 -> 2 -> 3 -> 3
         ListNode head = new ListNode(1);
         head.next = new ListNode(1);
         head.next.next = new ListNode(2);
@@ -51,7 +40,6 @@ public class L_83_RemoveDuplicatesFromSortedList {
 
         printList(result);
     }
-    // вспомогательный метод для вывода списка
     public static void printList(ListNode head) {
         ListNode temp = head;
         while (temp != null) {
