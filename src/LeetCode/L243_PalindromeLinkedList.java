aa9cc49b03198c50be4a1783a1513fd149724e04
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2026-01-15 21:17:08 -0800

    243

---
+++ src/LeetCode/L243_PalindromeLinkedList.java
@@ -0,0 +1,54 @@
+public class L243_PalindromeLinkedList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode firstHalf = head;
+        ListNode secondHalf = reverse(slow);
+
+        while (secondHalf != null) {
+            if (firstHalf.val != secondHalf.val) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+        return true;
+    }
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+    public static void main(String[] args) {
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(2);
+        ListNode d = new ListNode(1);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+
+        L243_PalindromeLinkedList sol = new L243_PalindromeLinkedList();
+        System.out.println(sol.isPalindrome(a));
+    }
+}
